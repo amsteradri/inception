f7aa89c5f8e60897d92ce7805033d8e1638a8d5c
amsteradri <amsteradri@gmail.com>
2024-12-05 12:47:29 +0100

    .env working

--- requirements/wordpress/script.sh
+++ requirements/wordpress/script.sh
@@ -9,23 +9,11 @@ if [ -f "/var/www/html/wp-config.php" ]; then
     exit 0
 fi
 
-# Update WP CLI
 wp cli update --allow-root
-
-# Install WP CLI
 wp core download --path=/var/www/html --locale=es_ES --allow-root
-
-# Create wp-config.php for the next installation of Wordpress
-wp config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-
-# Install Wordpress with CLI
-wp core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
-# Create a new user
-wp user create adgutier amsteradri@gmail.com --role=author --user_pass=pass --allow-root
-
-# Change the ownership of the directory to www-data(Nginx user)
+wp config create --dbname="$MARIADB_DATABASE" --dbuser="$MARIADB_USER" --dbpass="$MARIADB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --allow-root
+wp core install --url="$WORDPRESS_URL" --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --allow-root
+wp user create "$WORDPRESS_USER" "$WORDPRESS_USER_EMAIL" --role=author --user_pass="$WORDPRESS_USER_PASSWORD" --allow-root
 chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html
-
-# Execute the CMD from Dockerfile
 exec "$@"
